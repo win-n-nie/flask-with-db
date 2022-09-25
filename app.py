2da15ef58bb6e89f59c965655db40213bd8b12ad
win-n-nie <102179077+win-n-nie@users.noreply.github.com>
2022-09-25 18:23:11 -0400

    final update

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from flask import Flask, render_template
 import sqlite3
 import os
 
-# create a new flask app
+from sqlalchemy import false
+
 app = Flask(__name__)
 
-# create a function that will be called when the user accesses the root of the website
 def get_db_connection():
     dir = os.getcwd() + '/patients.db'
     print('dir:', dir)
-    conn = sqlite3.connect(dir) # create a connection to the database
-    conn.row_factory = sqlite3.Row # The line of code assigning sqlite3.Row to the row_factory of connection creates what some people call a 'dictionary cursor', - instead of tuples it starts returning 'dictionary' rows after fetchall or fetchone
-    # for more information about these two lines, good conversation on stackoverflow: https://stackoverflow.com/questions/44009452/what-is-the-purpose-of-the-row-factory-method-of-an-sqlite3-connection-object found there
+    conn = sqlite3.connect(dir) 
+    conn.row_factory = sqlite3.Row 
     return conn
 
 @app.route('/')
@@ -20,15 +19,15 @@ def index():
     patientListSql = db.execute('SELECT * FROM patient_table').fetchall()
     db.close()
     print('patientListSql:', patientListSql)
-    return render_template('page.html', listPatients=patientListSql) # note, these are two variables, patientsList is what we can then look up in the .html, and the patientsListSql is the actual data we are pulling from the sqlite db
-
+    return render_template('page.html', listPatients=patientListSql) 
 @app.route('/patients')
+
 def bootstrap():
     conn = get_db_connection()
     patientListSql = conn.execute('SELECT * FROM patient_table').fetchall()
     conn.close()
     print('patientListSql:', patientListSql)
-    return render_template('patients.html', listPatients=patientListSql) # note, these are two variables, patientsList is what we can then look up in the .html, and the patientsListSql is the actual data we are pulling from the sqlite db
+    return render_template('patients.html', listPatients=patientListSql) 
 
 
 if __name__ == '__main__':
